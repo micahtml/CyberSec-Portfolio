328d99cb24fa6e017eb699350f1b9c3dbf0a3e5d
micahtml <micahtml.booking@gmail.com>
2023-05-17 14:49:37 -0500

    initial commit

---
+++ Tools/Password Strength Check/standard_check.py
@@ -0,0 +1,49 @@
+import re
+
+
+def check_password_strength(password):
+    score = 0
+
+    # Check the length of the password
+    if len(password) >= 8:
+        score += 1
+
+    # Check for uppercase letters
+    if re.search(r"[A-Z]", password):
+        score += 1
+
+    # Check for lowercase letters
+    if re.search(r"[a-z]", password):
+        score += 1
+
+    # Check for digits
+    if re.search(r"\d", password):
+        score += 1
+
+    # Check for special characters
+    if re.search(r"\W", password):
+        score += 1
+
+    return score
+
+
+def rate_password_strength(score):
+    if score == 0:
+        return "Very Weak"
+    elif score == 1:
+        return "Weak"
+    elif score == 2:
+        return "Moderate"
+    elif score == 3:
+        return "Strong"
+    elif score == 4:
+        return "Very Strong"
+    else:
+        return "Invalid"
+
+
+# Example usage
+password = input("Enter your password: ")
+password_strength = check_password_strength(password)
+rating = rate_password_strength(password_strength)
+print("Password Strength: " + rating)

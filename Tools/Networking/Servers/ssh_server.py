9582f98cab956f1bae8d981c5d2f33f4fcac7753
Micah Thornton <48172545+micahtml@users.noreply.github.com>
2023-06-06 11:47:56 -0500

    debug

--- Tools/Networking/Servers/ssh_server.py
+++ Tools/Networking/Servers/ssh_server.py
@@ -9,7 +9,8 @@ import threading
 CWD = os.path.dirname(os.path.realpath(__file__))
 HOSTKEY = paramiko.RSAKey(filename=os.path.join(CWD, 'test_rsa.key'))
 
-class Server (paramiko.ServerInterface):
+
+class Server(paramiko.ServerInterface):
     def __init__(self):
         self.event = threading.Event()
 
@@ -22,6 +23,7 @@ class Server (paramiko.ServerInterface):
         if (username == 'tim') and (password == 'sekret'):
             return paramiko.AUTH_SUCCESSFUL
 
+
 if __name__ == '__main__':
     server = '192.168.1.207'
     ssh_port = 2222
@@ -32,36 +34,35 @@ if __name__ == '__main__':
         sock.listen(100)
         print('[+] Listening for connection ...')
         client, addr = sock.accept()
+        print('[+] Got a connection!', client, addr)  # Fixed typo here
+        bhSession = paramiko.Transport(client)  # Moved inside the `else` block
+        bhSession.add_server_key(HOSTKEY)
+        server = Server()
+        bhSession.start_server(server=server)
+
+        chan = bhSession.accept(20)
+        if chan is None:
+            print('*** No channel.')
+            sys.exit(1)
+
+        print('[+] Authenticated!')
+        print(chan.recv(1024))
+        chan.send('Welcome to bh_ssh')
+        try:
+            while True:
+                command = input("Enter command: ")
+                if command != 'exit':
+                    chan.send(command)
+                    r = chan.recv(8192)
+                    print(r.decode())
+                else:
+                    chan.send('exit')
+                    print('exiting')
+                    bhSession.close()
+                    break
+        except KeyboardInterrupt:
+            bhSession.close()
+
     except Exception as e:
         print('[-] Listen failed: ' + str(e))
         sys.exit(1)
-    else:
-        print('[+] GOt a conneciton!', client, addr)
-
-bhSession = paramiko.Transport(client)
-bhSession.add_server_key(HOSTKEY)
-server = Server()
-bhSession.start_server(server=server)
-
-chan = bhSession.accept(20)
-if chan is None:
-    print('*** No channel.')
-    sys.exit(1)
-
-print('[+] Authenticated!')
-print(chan.recv(1024))
-chan.send('Welcome to bh_ssh')
-try:
-    while True:
-        command = input("Enter command: ")
-        if command != 'exit':
-            chan.send(command)
-            r = chan.recv(8192)
-            print(r.decode())
-        else:
-            chan.send('exit')
-            print('exiting')
-            bhSession.close()
-            break
-except KeyboardInterrupt:
-    bhSession.close()

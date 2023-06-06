56c2a9f3d5d569db6802f4868f10a9f2c1ef1048
Micah Thornton <48172545+micahtml@users.noreply.github.com>
2023-06-06 08:42:31 -0500

    debug first draft

--- Tools/Networking/Clients/TCP_client.py
+++ Tools/Networking/Clients/TCP_client.py
@@ -5,17 +5,23 @@ import socket
 target_host = "www.google.com"
 target_port = 80
 
-#create a socket object
+# Create a socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#connect the client
-client.connect(target_host, target_port)
+# Connect the client to the target host and port
+# Corrected syntax for the connect method
+client.connect((target_host, target_port))
 
-#send some data
-client.send(b"GET / HTTP/1.1\r\nHost:google.com\r\n\r\n")
+# Send some data
+# Added a space after the colon in the Host header
+client.send(b"GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n")
 
-#recieve some data
+# Receive some data
 dataresponse = client.recv(4096)
 
-print(response.decode())
+# Print the received data
+# Changed the variable name from 'response' to 'dataresponse'
+print(dataresponse.decode())
+
+# Close the socket connection
 client.close()
